e1f3aeb30faa89b50943dff5f5a8e510ec71681f
ub-victor <victoireushindi371@gmail.com>
2025-09-17 12:49:27 +0200

    Initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/19/4071e13aa7930010156da5d5b6892cd4
@@ -0,0 +1,10 @@
+package day5;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MyJavaProject/src/day5/ForLoopExample.java
@@ -0,0 +1,23 @@
+package day5;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		
+//		for(int i = 1; i<=10; i++) {
+//			System.out.println(i);
+//		}
+//		System.out.println("So this is the end of our for loop");
+		
+		for(int i = 1; i<=10; i++) {
+			
+			if(i%2 == 0) {
+				System.out.println(i);
+			}
+			
+		}
+		System.out.println("So this is the end of our for loop for even number");
+
+	}
+
+}
