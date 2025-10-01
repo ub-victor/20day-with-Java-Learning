692db87b937802130e3a90fed3bcb33118a9daa8
ub-victor <victoireushindi371@gmail.com>
2025-10-01 17:47:00 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise19.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise19 {
+    public static void main(String[] args) {
+        System.out.println("This is line 1.");
+        System.out.println("This is line 2.");
+    }
+}
