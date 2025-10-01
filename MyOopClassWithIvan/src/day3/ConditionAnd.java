ca29334303c65974673d074b65b8f55912a1e02e
ub-victor <victoireushindi371@gmail.com>
2025-10-01 20:27:01 +0200

    Intro to oop

--- MyOopClassWithIvan/src/day3/ConditionAnd.java
+++ MyOopClassWithIvan/src/day3/ConditionAnd.java
@@ -5,12 +5,22 @@ public class ConditionAnd {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the student's mark: "); 
+        
+        
+        if(mark> 100 || mark<0) {
+        	
+        	System.out.print("Enter the student's mark: "); 
+        	
+        	System.out.println("The entered mark is: " + mark);
+        	
+        	int mark = scanner.nextInt();
+        	
+        }
 
-        // Read the integer mark entered by the user
+        
         int mark = scanner.nextInt();
 
-        // Display the entered mark
+        
         System.out.println("The entered mark is: " + mark);
 
         // Close the scanner to release system resources
