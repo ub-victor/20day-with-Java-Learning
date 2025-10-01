ec24a14a8abb935687354ef41cb2bda9ea8bcbdd
ub-victor <victoireushindi371@gmail.com>
2025-10-01 17:41:28 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise13.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise13 {
+    public static void main(String[] args) {
+        String name = "David";
+        System.out.println("My name is " + name);
+    }
+}
