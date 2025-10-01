d755448c1adb353a7113619cb596b7a0b80e7b3b
ub-victor <victoireushindi371@gmail.com>
2025-10-01 16:19:10 +0200

    Intro to oop

--- MyOopClassWithIvan/src/holydayJavaExercises/DeclareVariableWithInitialization.java
+++ MyOopClassWithIvan/src/holydayJavaExercises/DeclareVariableWithInitialization.java
@@ -3,7 +3,8 @@ package holydayJavaExercises;
 public class DeclareVariableWithInitialization {
 
 	public static void main(String[] args) {
-		
+	     String name = "David";
+	     System.out.println("My name is: " + name);
 
 	}
 
---
+++ MyOopClassWithIvan/src/holydayJavaExercises/Declareaninteger.java
@@ -0,0 +1,11 @@
+package holydayJavaExercises;
+
+public class Declareaninteger {
+
+	public static void main(String[] args) {
+		int year = 2024;
+        System.out.println("The year is " + year);
+
+	}
+
+}
