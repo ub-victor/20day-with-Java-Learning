4e6c9ba2cb09b2a986c7f9b9831c8f476c730d22
ub-victor <victoireushindi371@gmail.com>
2025-10-01 17:52:56 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise21.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise21 {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("Sum: " + (a + b));
+    }
+}
---
+++ MyOopClassWithIvan/src/exercises/Exercise22.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise22 {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("Difference: " + (a - b));
+    }
+}
