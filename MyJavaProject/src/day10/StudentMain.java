27e84452645df4e00edd97fd398fd19305c31e58
ub-victor <victoireushindi371@gmail.com>
2025-10-01 19:03:40 +0200

    Intro to oop

--- MyJavaProject/src/day10/StudentMain.java
+++ MyJavaProject/src/day10/StudentMain.java
@@ -3,12 +3,29 @@ package day10;
 public class StudentMain {
 
 	public static void main(String[] args) {
+
 		
+		// Create object with reference variable
+		
+/*
 		Student s = new Student();
 		s.sid = 1001;
 		s.sname = "Victoire";
 		s.grade = 'A';
 		s.printStudentData();
+		
+*/
+		
+		new Student();
+		
+		new Student().sid = 1002;
+		new Student().sname = "Kyle";
+		new Student().grade = 'B';
+		
+		new Student().printStudentData();
+		
+		
+		
 
 	}
 
