21d67b4f6db08f1b9c13effe3cfd21e010a731a0
ub-victor <victoireushindi371@gmail.com>
2025-10-01 17:46:12 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise18.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise18 {
+    public static void main(String[] args) {
+        char grade = 'A';
+        System.out.println("I got an " + grade + " in my programming class.");
+    }
+}
