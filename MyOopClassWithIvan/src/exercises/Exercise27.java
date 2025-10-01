3befea6af28bc5e7ea2f2f867ef937fa2f9629a2
ub-victor <victoireushindi371@gmail.com>
2025-10-01 17:56:59 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise27.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Exercise27 {
+    public static void main(String[] args) {
+        int length = 5, width = 3;
+        int area = length * width;
+        System.out.println("Area of rectangle: " + area);
+    }
+}
---
+++ MyOopClassWithIvan/src/exercises/Exercise28.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Exercise28 {
+    public static void main(String[] args) {
+        int count = 0;
+        count++;
+        System.out.println("After increment: " + count);
+    }
+}
