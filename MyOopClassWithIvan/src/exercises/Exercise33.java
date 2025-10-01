ac2dcf17d1983b56e077355cb2e07337ba253fd6
ub-victor <victoireushindi371@gmail.com>
2025-10-01 18:01:00 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise33.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Exercise33 {
+    public static void main(String[] args) {
+        int total = 20;
+        total /= 4;
+        System.out.println("Total after dividing by 4: " + total);
+    }
+}
