96dfb0d73cb519faea20064fc06d896e7dd40bd1
ub-victor <victoireushindi371@gmail.com>
2025-10-01 17:47:25 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise20.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise20 {
+    public static void main(String[] args) {
+        System.out.print("Message 1 ");
+        System.out.print("Message 2");
+    }
+}
