88e289fc14633ee45600aea066f2336b79f08016
ub-victor <victoireushindi371@gmail.com>
2025-10-01 18:06:43 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise38.java
@@ -0,0 +1,7 @@
+package exercises;
+
+public class Exercise38 {
+    public static void main(String[] args) {
+        System.out.println(10 == 10);
+    }
+}
---
+++ MyOopClassWithIvan/src/exercises/Exercise39.java
@@ -0,0 +1,7 @@
+package exercises;
+
+public class Exercise39 {
+    public static void main(String[] args) {
+        System.out.println(10 != 5);
+    }
+}
