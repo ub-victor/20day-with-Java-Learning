190c08c76d44b978e874d3428a69e823fe63e2ef
ub-victor <victoireushindi371@gmail.com>
2025-10-01 18:00:15 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise31.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Exercise31 {
+    public static void main(String[] args) {
+        int score = 10;
+        score -= 3;
+        System.out.println("Score after subtracting 3: " + score);
+    }
+}
---
+++ MyOopClassWithIvan/src/exercises/Exercise32.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Exercise32 {
+    public static void main(String[] args) {
+        int value = 7;
+        value *= 2;
+        System.out.println("Value after multiplying by 2: " + value);
+    }
+}
