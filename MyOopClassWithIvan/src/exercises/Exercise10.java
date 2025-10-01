cfa58e8b86dd790b70c16cbe4a49b035cf562d63
ub-victor <victoireushindi371@gmail.com>
2025-10-01 17:38:45 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise10.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise10 {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+        System.out.println("PI = " + PI);
+    }
+}
