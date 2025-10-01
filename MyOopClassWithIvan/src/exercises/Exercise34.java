eb4dd29a84eb3a4a6816ea837939504bde879990
ub-victor <victoireushindi371@gmail.com>
2025-10-01 18:01:50 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise34.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Exercise34 {
+    public static void main(String[] args) {
+        double item1 = 2.50, item2 = 3.75, item3 = 1.99;
+        double total = item1 + item2 + item3;
+        System.out.println("Total cost: " + total);
+    }
+}
