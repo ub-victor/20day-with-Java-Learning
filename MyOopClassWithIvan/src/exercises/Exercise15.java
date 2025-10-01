6f5cc8ea5e4c68e24000192a07d4b18320457292
ub-victor <victoireushindi371@gmail.com>
2025-10-01 17:43:31 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise15.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise15 {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+        System.out.println("The value of PI is " + PI);
+    }
+}
---
+++ MyOopClassWithIvan/src/exercises/Exercise16.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise16 {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        System.out.println("Is Java fun? " + isJavaFun);
+    }
+}
