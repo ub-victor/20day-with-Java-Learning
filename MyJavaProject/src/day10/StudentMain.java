ab9b821337592bb4b54e41ce6863ece3ab81c633
ub-victor <victoireushindi371@gmail.com>
2025-10-01 18:52:22 +0200

    Intro to oop

---
+++ MyJavaProject/src/day10/StudentMain.java
@@ -0,0 +1,15 @@
+package day10;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		
+		Student s = new Student();
+		s.sid = 1001;
+		s.sname = "Victoire";
+		s.grade = 'A';
+		s.printStudentData();
+
+	}
+
+}
