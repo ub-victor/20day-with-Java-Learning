8ecf88e41acc1c521e8f1357ab9cf42e3a419f03
ub-victor <victoireushindi371@gmail.com>
2025-10-01 18:16:56 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise50.java
@@ -0,0 +1,11 @@
+package exercises;
+
+public class Exercise50 {
+    public static void main(String[] args) {
+        int a = 15, b = 20;
+
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a < b: " + (a < b));
+    }
+}
