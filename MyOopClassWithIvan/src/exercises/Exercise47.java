78b651446790558207ea449876c5da0d31a3f801
ub-victor <victoireushindi371@gmail.com>
2025-10-01 18:13:06 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise47.java
@@ -0,0 +1,10 @@
+package exercises;
+
+public class Exercise47 {
+    public static void main(String[] args) {
+        double F = 72.0;
+        double C = (F - 32) * 5.0 / 9.0;
+
+        System.out.println(F + "°F is " + C + "°C");
+    }
+}
---
+++ MyOopClassWithIvan/src/exercises/Exercise48.java
@@ -0,0 +1,16 @@
+package exercises;
+
+public class Exercise48 {
+    public static void main(String[] args) {
+        String name = "David";
+        int age = 22;
+        char grade = 'A';
+        double average = 89.5;
+
+        System.out.println("---- Student Info ----");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("Average: " + average);
+    }
+}
