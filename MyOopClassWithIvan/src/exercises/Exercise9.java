5c9ee7d6b85a41b263a63f9b0bdb94b698c828c3
ub-victor <victoireushindi371@gmail.com>
2025-10-01 17:38:15 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise9.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        int number;
+        number = 100;
+        System.out.println("Number: " + number);
+    }
+}
