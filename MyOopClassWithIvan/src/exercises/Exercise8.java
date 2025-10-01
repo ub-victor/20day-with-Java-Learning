5f94b2ab4604fbb5e7367807db6e1bcb4fc036e9
ub-victor <victoireushindi371@gmail.com>
2025-10-01 17:37:23 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise8.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        float value = 10.5f; // must use 'f' suffix
+        System.out.println("Float value: " + value);
+    }
+}
