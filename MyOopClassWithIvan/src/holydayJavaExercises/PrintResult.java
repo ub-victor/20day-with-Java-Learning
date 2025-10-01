cb1017680bb984dd056d8f305c62a6bc39d08833
ub-victor <victoireushindi371@gmail.com>
2025-10-01 16:22:01 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/holydayJavaExercises/PrintResult.java
@@ -0,0 +1,11 @@
+package holydayJavaExercises;
+
+public class PrintResult {
+
+	public static void main(String[] args) {
+		int a = 10, b = 5;
+        System.out.println("Multiplication: " + (a * b));
+
+	}
+
+}
