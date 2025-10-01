c3ce8215ca6cf32442d80ebc30c920e05dbfd3ae
ub-victor <victoireushindi371@gmail.com>
2025-10-01 18:48:25 +0200

    Intro to oop

--- MyJavaProject/src/day10/Student.java
+++ MyJavaProject/src/day10/Student.java
@@ -1,5 +1,12 @@
 package day10;
 
 public class Student {
+	int sid;
+	String sname;
+	char grade;
+	
+	void printStudentData(){
+		System.out.println("Student id: "+ sid + ", Name:" + sname+ ", Grade:" + grade );
+	}
 
 }
