86c57e31cb604265bffb22661e374f23e8d08f6f
ub-victor <victoireushindi371@gmail.com>
2025-10-01 18:10:14 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise44.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise44 {
+    public static void main(String[] args) {
+        int age = 20; // from earlier exercises, just assume age = 20
+        System.out.println(age >= 18);
+    }
+}
