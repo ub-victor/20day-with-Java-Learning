fd33cd1960787f936193cdbfd1a78f933fc9fbdd
ub-victor <victoireushindi371@gmail.com>
2025-10-01 18:02:50 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/exercises/Exercise35.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Exercise35 {
+    public static void main(String[] args) {
+        double F = 100;
+        double C = (F - 32) * 5.0 / 9.0;
+        System.out.println(F + "°F is " + C + "°C");
+    }
+}
