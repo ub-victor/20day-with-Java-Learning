a779911272c9eef68d98b1e26687e48d5b3a0e91
ub-victor <victoireushindi371@gmail.com>
2025-10-07 23:32:06 +0200

    Intro to oop

--- MyJavaProject/src/day11/ConstructorExample.java
+++ MyJavaProject/src/day11/ConstructorExample.java
@@ -4,12 +4,12 @@ public class ConstructorExample {
 	
 	int x,y;
 	
-	ConstructorExample(){ // know as Default constructor
+	ConstructorExample(){ // know as Default constructor it takes no parms
 		
 	}
 
 	public static void main(String[] args) {
-		int 
+		
 
 	}
 
