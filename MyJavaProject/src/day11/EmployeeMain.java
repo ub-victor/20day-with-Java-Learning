331331fe81574519cd0b4ba29c34dbb4700b9272
ub-victor <victoireushindi371@gmail.com>
2025-10-07 20:23:27 +0200

    Intro to oop

--- MyJavaProject/src/day11/EmployeeMain.java
+++ MyJavaProject/src/day11/EmployeeMain.java
@@ -2,7 +2,12 @@ package day11;
 
 public class EmployeeMain {
 	public static void main() {
-	
+		
+		Employee e1 = new Employee();
+		
+		e1.eid = 101;
+		e1.ename = "Victo";
+		e1.sal = 345554.34;
 	
 	}
 	
