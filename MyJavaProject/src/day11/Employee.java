36479965ac92e11e170127576b8f0c2bc07bb954
ub-victor <victoireushindi371@gmail.com>
2025-10-07 20:16:18 +0200

    Intro to oop

---
+++ MyJavaProject/src/day11/Employee.java
@@ -0,0 +1,15 @@
+package day11;
+
+public class Employee {
+	
+	int eid;
+	String ename;
+	double sal;
+	
+	void displayEmpData() {
+		System.out.println("Employee ID: "+ eid);
+		System.out.println("Employee Name: "+ ename);
+		System.out.println("Employee Salary: "+ sal);
+	}
+
+}
