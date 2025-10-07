95d28e3562a2d04e180b2cd344fe352a681b4d90
ub-victor <victoireushindi371@gmail.com>
2025-10-07 23:54:32 +0200

    Intro to oop

--- MyJavaProject/src/day11/ConstructorExample.java
+++ MyJavaProject/src/day11/ConstructorExample.java
@@ -4,12 +4,12 @@ public class ConstructorExample {
 	
 	int x,y;
 	
-	/*ConstructorExample(){ // know as Default constructor it takes no parms
+	ConstructorExample(){ // know as Default constructor it takes no parms
 		x = 100;
 		y=200;
 	}
 	
-	*/
+	
 	ConstructorExample(int i , int j){
 		x=i;
 		y=j;
@@ -21,15 +21,15 @@ public class ConstructorExample {
 
 	public static void main(String[] args) {
 		
-		/*
+		
 		
 		ConstructorExample ce = new ConstructorExample();
 		
 		System.out.println("Sum of the numbers is: "+ ce.sum());
-		*/
 		
-		ConstructorExample ce = new ConstructorExample(300, 300);
-		System.out.println(ce.sum());
+		
+		ConstructorExample cee = new ConstructorExample(300, 300);
+		System.out.println(cee.sum());
 		
 
 	}
