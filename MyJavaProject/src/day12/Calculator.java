ca63c6c7761ace33cc5ae01a1e2cc2d7a509768d
ub-victor <victoireushindi371@gmail.com>
2025-10-08 19:14:39 +0200

    Intro to oop

--- MyJavaProject/src/day12/Calculator.java
+++ MyJavaProject/src/day12/Calculator.java
@@ -23,7 +23,13 @@ public class Calculator {
 			
 	}
 	
-void add (int x, double y) {
+	void add (int x, double y) {
+			
+			System.out.println(x+y);
+				
+		}
+	
+	void add (double x, int y) {
 		
 		System.out.println(x+y);
 			
