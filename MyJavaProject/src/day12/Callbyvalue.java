7a5ee89511e9ba3843d3e7e1c7481cd95e136512
ub-victor <victoireushindi371@gmail.com>
2025-10-08 14:14:05 +0200

    Intro to oop

--- MyJavaProject/src/day12/Callbyvalue.java
+++ MyJavaProject/src/day12/Callbyvalue.java
@@ -6,11 +6,11 @@ public class Callbyvalue {
 		TestMethod tm = new TestMethod();
 		
 		int num = 100;
-		System.out.println("Value before the method call: ");
+		System.out.println("Value before the method call: " + num);
 		
 		tm.modifyValue(num);
 		
-		System.out.println("Value after the method call: ",+ n);
+		System.out.println("Value after the method call: "+ num);
 		
 		
 
