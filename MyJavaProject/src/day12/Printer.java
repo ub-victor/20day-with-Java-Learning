97aa4f5a66558a53cf0b3cf152a973d42bb072aa
ub-victor <victoireushindi371@gmail.com>
2025-10-08 21:12:08 +0200

    Intro to oop

--- MyJavaProject/src/day12/Printer.java
+++ MyJavaProject/src/day12/Printer.java
@@ -1,6 +1,18 @@
 package day12;
 
 public class Printer {
+	
+	void print(String document) {
+		
+		System.out.println("Printing String document: "+ document);
+		
+	}
+	
+	void print(int document) {
+			
+			System.out.println("Printing String document: "+ document);
+			
+		}
 
 	public static void main(String[] args) {
 		
