adbcfc93e599481222c96d6c9f25b84050b61d85
ub-victor <victoireushindi371@gmail.com>
2025-10-08 18:05:08 +0200

    Intro to oop

--- MyJavaProject/src/day12/Calculator.java
+++ MyJavaProject/src/day12/Calculator.java
@@ -2,6 +2,24 @@ package day12;
 
 public class Calculator {
 	
+	int a = 10, b = 20;
 	
+	void add () {
+		
+	}
+	
+	void add () {
+			
+		}
+
+
+	void add () {
+		
+	}
+
+	
+	void add () {
+		
+	}
 
 }
