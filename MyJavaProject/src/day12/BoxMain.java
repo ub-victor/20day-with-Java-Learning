c34774f0defbc06538ed0df7e0ce56bf7309b404
ub-victor <victoireushindi371@gmail.com>
2025-10-09 01:54:06 +0200

    Intro to oop

--- MyJavaProject/src/day12/BoxMain.java
+++ MyJavaProject/src/day12/BoxMain.java
@@ -9,12 +9,13 @@ public class BoxMain {
 		
 		Box bo = new Box(15.5, 8.6, 5.4);
 		
-		
+		Box cube = new Box(3);
 		
 		
 		
 		System.out.println(b.volume());
 		System.out.println(bo.volume());
+		System.out.println(cube.volume());
 
 	}
 
