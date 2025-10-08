3ab62eb0e7c72d54e75bc4c210debbb7441dcbe2
ub-victor <victoireushindi371@gmail.com>
2025-10-08 15:09:57 +0200

    Intro to oop

--- MyJavaProject/src/day12/CallByReference.java
+++ MyJavaProject/src/day12/CallByReference.java
@@ -9,6 +9,12 @@ public class CallByReference {
 		int origNum = 100;
 		System.out.println("Value before method call: "+ origNum);
 		
+		tm.modifyObject(tm);
+		
+		System.out.println("Value after method call: "+ origNum);
+		
+		
+		
 	}
 
 }
