6c5a128d68ebd0d661863faa966a9c959129ca87
ub-victor <victoireushindi371@gmail.com>
2025-10-09 02:12:17 +0200

    Intro to oop

--- MyJavaProject/src/day12/PassParamsToMain.java
+++ MyJavaProject/src/day12/PassParamsToMain.java
@@ -13,3 +13,13 @@ public class PassParamsToMain {
 	}
 
 }
+
+/*
+ * To give the main method value, as it declare to have String array
+ * 1. Right click then Run Configuration then
+ * 2. In the main you check where written Main class, you copy it 
+ * 3. search for the main class then okay
+ * 4. click of the Arguments
+ * 
+ * 
+ * */
