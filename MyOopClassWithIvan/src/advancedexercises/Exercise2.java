5e5ae217d94c4b5cf0c1bf5c045111eae0801f8a
ub-victor <victoireushindi371@gmail.com>
2025-10-15 04:00:00 +0200

    Intro to oop

---
+++ MyOopClassWithIvan/src/advancedexercises/Exercise2.java
@@ -0,0 +1,23 @@
+package advancedexercises;
+import java.util.Scanner;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a year");
+		int year = sc.nextInt();
+		
+		if((year % 4 == 0 && year % 100 !=0) || year % 400 == 0) {
+			System.out.print(year + " Is a leap year.");
+		}else {
+			System.out.println(year + " Is Not a leap year.");
+		}
+		
+		sc.close();		
+
+	}
+
+}
