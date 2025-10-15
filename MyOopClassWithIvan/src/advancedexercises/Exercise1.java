18a04b5f3891d6136a1c0a6e284498146b9ecd02
ub-victor <victoireushindi371@gmail.com>
2025-10-15 03:26:22 +0200

    Intro to oop

--- MyOopClassWithIvan/src/advancedexercises/Exercise1.java
+++ MyOopClassWithIvan/src/advancedexercises/Exercise1.java
@@ -7,6 +7,19 @@ public class Exercise1 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter side1: ");
+		int a = sc.nextInt();
+		System.out.println("Enter side2: ");
+		int b = sc.nextInt();
+		System.out.println("Enter side3: ");
+		int c = sc.nextInt();
+		
+		if (a+b > c && a + c > b && b+c> a) {
+			System.out.println("These sides form a triangle");
+		}else {
+			System.out.println("These sides do Not form a triangle");
+		}
+		
+		sc.close();
 
 	}
 
