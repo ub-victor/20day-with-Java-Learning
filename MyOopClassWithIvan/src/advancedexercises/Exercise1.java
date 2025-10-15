73bc7f20567520749e2b3cbf625d80136784a3e7
ub-victor <victoireushindi371@gmail.com>
2025-10-15 03:21:19 +0200

    Intro to oop

--- MyOopClassWithIvan/src/advancedexercises/Exercise1.java
+++ MyOopClassWithIvan/src/advancedexercises/Exercise1.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class Exercise1 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in)
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter side1: ");
 
 	}
 
