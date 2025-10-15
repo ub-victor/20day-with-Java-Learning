1a9527755141cf917429994f022029525836fe6c
ub-victor <victoireushindi371@gmail.com>
2025-10-15 18:51:42 +0200

    If Statement with scanner

--- MyOopClassWithIvan/src/advancedexercises/Exercise3.java
+++ MyOopClassWithIvan/src/advancedexercises/Exercise3.java
@@ -1,9 +1,14 @@
 package advancedexercises;
+import java.util.Scanner;
 
 public class Exercise3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a character: ");
+		char ch = sc.next().charAt(0);
+		
 
 	}
 
