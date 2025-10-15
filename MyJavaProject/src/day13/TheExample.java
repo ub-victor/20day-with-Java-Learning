457803b9939254a3463ae2f6d4634a5c943c51ec
ub-victor <victoireushindi371@gmail.com>
2025-10-15 20:06:52 +0200

    If Statement with scanner

--- MyJavaProject/src/day13/TheExample.java
+++ MyJavaProject/src/day13/TheExample.java
@@ -4,9 +4,13 @@ public class TheExample {
 	
 	int a,b  // Class variables
 	
-	void setData(int x, int y) {
+	void setData(int x, int y) { // x and be are Local Variables
 		a = x;
 		b = y;
+	}
+	
+	void displayData() {
+		System.out.println(a + " " + b);
 	}
 
 	public static void main(String[] args) {
