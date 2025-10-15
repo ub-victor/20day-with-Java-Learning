5f01c31c9723c14ad6332abb435d11d9049aec2e
ub-victor <victoireushindi371@gmail.com>
2025-10-15 04:00:36 +0200

    Intro to oop

--- MyOopClassWithIvan/src/advancedexercises/Exercise2.java
+++ MyOopClassWithIvan/src/advancedexercises/Exercise2.java
@@ -7,7 +7,7 @@ public class Exercise2 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Enter a year");
+		System.out.print("Enter a year: ");
 		int year = sc.nextInt();
 		
 		if((year % 4 == 0 && year % 100 !=0) || year % 400 == 0) {
