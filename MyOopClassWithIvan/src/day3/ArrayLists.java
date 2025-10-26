2bf32c3182862c4eb1752f1fbc2be532a046d829
ub-victor <victoireushindi371@gmail.com>
2025-10-26 02:25:21 +0200

    Method

--- MyOopClassWithIvan/src/day3/ArrayLists.java
+++ MyOopClassWithIvan/src/day3/ArrayLists.java
@@ -1,11 +1,14 @@
 package day3;
+import java.util.ArrayList;
 
 public class ArrayLists {
 
 	public static void main(String[] args) {
 		
 		
-		// 
+		// ArrayList = A resizeable array that stores objects (autoboxing).
+		
+		ArrayList<Interger> list = new ArrayList<>();
 
 
 	}
