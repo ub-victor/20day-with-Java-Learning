4d6cd56d14461cf92b613bd17b4e7299db33c4ce
ub-victor <victoireushindi371@gmail.com>
2025-10-26 02:41:31 +0200

    Method

--- MyOopClassWithIvan/src/day3/UserEnterFood.java
+++ MyOopClassWithIvan/src/day3/UserEnterFood.java
@@ -1,10 +1,17 @@
 package day3;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class UserEnterFood {
 
 	public static void main(String[] args) {
 		
 		Scanner sr = new Scanner(System.in);
+		
+		ArrayList<String> foods = new ArrayList<>();
+		
+		System.out.print("Enter the # of food you would like: ");
 
 	}
 
