b77bb84582655a62784b78cbeaf6556a60ab24e9
ub-victor <victoireushindi371@gmail.com>
2025-11-05 20:47:43 +0200

    Obj Intro

--- back-management-system/src/bk/Loan.java
+++ back-management-system/src/bk/Loan.java
@@ -6,7 +6,8 @@ public class Loan {
 	int loan;
 	double loanId;
 	double restedAmount;
-	double interestAmount;
+	double requestedAmount;
+	double interestRate;
 	double savingAmount;
 	public static void main(String[] args) {
 		
