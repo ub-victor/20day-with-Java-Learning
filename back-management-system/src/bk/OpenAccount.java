5550acd4fb0116c604c5423648d3f92b70d522be
ub-victor <victoireushindi371@gmail.com>
2025-11-05 19:53:53 +0200

    Obj Intro

--- back-management-system/src/bk/OpenAccount.java
+++ back-management-system/src/bk/OpenAccount.java
@@ -4,12 +4,18 @@ public class OpenAccount {
 	
 	int accountNumber;
 	String accountHolderName;
-	String ;
+	String address;
+	String phoneNumber;
+	String email;
+	String initialBalance;
 	
-
-	//public static void main(String[] args) {
-		
-
-	//}
+	public void DisplayAccountDetails() {
+		System.out.println("Account Number: " + accountNumber);
+		System.out.println("Account Holder Name: " + accountHolderName);
+		System.out.println("Address: " + address);
+		System.out.println("Phone Number: " + phoneNumber);
+		System.out.println("Email: " + email);
+		System.out.println("Initial Balance: " + initialBalance);
+	}
 
 }
