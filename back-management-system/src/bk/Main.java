1b2329f39bdff729bd0708173c4355a10cbd6286
ub-victor <victoireushindi371@gmail.com>
2025-11-05 19:55:59 +0200

    Obj Intro

--- back-management-system/src/bk/Main.java
+++ back-management-system/src/bk/Main.java
@@ -3,9 +3,16 @@ package bk;
 public class Main {
 
 	public static void main(String[] args) {
+
+		OpenAccount account = new OpenAccount();
 		
-		OpenAccount = new 
-		
+		account.accountNumber = 123456;
+		account.accountHolderName = "John Doe";
+		account.address = "123 Main St, Anytown, USA";
+		account.phoneNumber = "555-1234";
+		account.email = "victoireushindi371@gmail.com";
+		account.initialBalance = "1000.00";
+		account.DisplayAccountDetails();
 
 	}
 
