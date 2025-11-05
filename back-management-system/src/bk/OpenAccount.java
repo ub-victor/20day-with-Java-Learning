124adad8dbef5a25aa542dfd04683418f1094fbe
ub-victor <victoireushindi371@gmail.com>
2025-11-05 19:46:00 +0200

    Obj Intro

--- back-management-system/src/bk/OpenAccount.java
+++ back-management-system/src/bk/OpenAccount.java
@@ -1,10 +1,15 @@
 package bk;
 
 public class OpenAccount {
+	
+	int accountNumber;
+	String accountHolderName;
+	String ;
+	
 
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 		
 
-	}
+	//}
 
 }
