3c9ed2dfb9cc607781e04438c89a98221200e4b6
ub-victor <victoireushindi371@gmail.com>
2025-11-06 12:05:14 +0200

    encapsulation

--- MyJavaProject/src/day13/PersonMain.java
+++ MyJavaProject/src/day13/PersonMain.java
@@ -6,8 +6,10 @@ public class PersonMain {
 		
 		Person p = new Person();
 //		p.setData("Victoire", 20);
-		p.name = "Victoire";
-		p.age = 20;
+//		p.name = "Victoire";
+//		p.age = 20;
+		
+		// to set the name or use the other class variable here we will do this
 		
 		
 		p.displayPersonDetails();
