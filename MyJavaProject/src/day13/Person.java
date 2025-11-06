f5adb66f292b4d8200010dc61d2066ff85840786
ub-victor <victoireushindi371@gmail.com>
2025-11-06 13:19:33 +0200

    encapseulation

--- MyJavaProject/src/day13/Person.java
+++ MyJavaProject/src/day13/Person.java
@@ -12,6 +12,8 @@ public class Person {
 	// So as the variable are private how to set they data?
 	// We can set them using  methods for each class
 	
+	//Here we were doing setter and getter manually then here is the shortcut built in IDE 
+	/*
 	void setName(String name) {
 		this.name = name;		
 	}
@@ -34,10 +36,27 @@ public class Person {
 		return age; 
 	}
 	
-
+*/
+	
 	
 	void displayPersonDetails() {
 		System.out.println("Person details : "+ name + " is " + age + " years old");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 }
