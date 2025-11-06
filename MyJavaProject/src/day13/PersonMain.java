3014d4d1ed771e811ecf0c0fe0c5b5c030ab2967
ub-victor <victoireushindi371@gmail.com>
2025-11-06 11:39:36 +0200

    encapsulation

--- MyJavaProject/src/day13/PersonMain.java
+++ MyJavaProject/src/day13/PersonMain.java
@@ -5,10 +5,10 @@ public class PersonMain {
 	public static void main(String[] args) {
 		
 		Person p = new Person();
+//		p.setData("Victoire", 20);
+		p.name = "Victoire";
+		p.age = 20;
 		
-//		p.name = "Victoire";
-//		p.age = 20;
-		p.setData("Victoire", 20);
 		
 		p.displayPersonDetails();
 
