34ef2f61af2dd588365b0b13010e33e255bb7daa
ub-victor <victoireushindi371@gmail.com>
2025-11-10 14:10:42 +0200

    Enpsulation

--- MyJavaProject/src/day13/StaticExample.java
+++ MyJavaProject/src/day13/StaticExample.java
@@ -2,8 +2,8 @@ package day13;
 
 public class StaticExample {
 	
-	int a= 10;
-	int b = 20;
+	static int a= 10;
+	static int b = 20;
 	
 	void m1(){
 		System.out.println("We are in m1 static method");
