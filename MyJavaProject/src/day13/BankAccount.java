bc111f16f11e5ee8ee70d6e045ba87034cfc811e
ub-victor <victoireushindi371@gmail.com>
2025-11-10 16:28:04 +0200

    Enpsulation

--- MyJavaProject/src/day13/BankAccount.java
+++ MyJavaProject/src/day13/BankAccount.java
@@ -4,6 +4,11 @@ public class BankAccount {
 	
 	// Static variable to keep track of total account created
 	static int totalAccounts = 0;
+	
+	static int generateAccountNumber() {
+		
+		return 
+	}
 
 	public static void main(String[] args) {
 		
