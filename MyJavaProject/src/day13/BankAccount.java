cf0ef316489a422adea8c0f5290e94aba7c049d2
ub-victor <victoireushindi371@gmail.com>
2025-11-10 16:51:21 +0200

    Enpsulation

--- MyJavaProject/src/day13/BankAccount.java
+++ MyJavaProject/src/day13/BankAccount.java
@@ -14,7 +14,11 @@ public class BankAccount {
 	
 	private int accountNum;
 	private String accholderName;
-	private double accountNum;
+	private double balance;
+	
+	public BankAccount() {
+		
+	}
 
 	public static void main(String[] args) {
 		
