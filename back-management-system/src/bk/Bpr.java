c4ff70522a60f6d65420eb0192f354f210bf3977
ub-victor <victoireushindi371@gmail.com>
2025-11-12 19:26:50 +0200

    inheritace

--- back-management-system/src/bk/Bpr.java
+++ back-management-system/src/bk/Bpr.java
@@ -1,5 +1,28 @@
 package bk;
 
 public class Bpr {
-    
+    private int bankId;
+    private String bankName;
+
+    public Bpr(int bankId, String bankName) {
+        this.bankId = bankId;
+        this.bankName = bankName;
+    }
+
+    // getter and setter methods
+    public int getBankId() {
+        return bankId;
+    }
+
+    public void setBankId(int bankId) {
+        this.bankId = bankId;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
 }
