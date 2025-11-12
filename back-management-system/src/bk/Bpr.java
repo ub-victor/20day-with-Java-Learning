52208a2cc4f98bd855f6c626076a6c36223eee58
ub-victor <victoireushindi371@gmail.com>
2025-11-12 19:30:30 +0200

    inheritace

--- back-management-system/src/bk/Bpr.java
+++ back-management-system/src/bk/Bpr.java
@@ -1,13 +1,13 @@
 package bk;
 
-public class Bpr {
+public class Bpr extends Bank {
     private String bprAddress;
 
 
-    public Bpr(String bprAddress) {
+    public Bpr(int bankId, String bankName, String bprAddress) {
+        super(bankId, bankName);
         this.bprAddress = bprAddress;
     }
-
     public String getBprAddress() {
         return bprAddress;
     }
