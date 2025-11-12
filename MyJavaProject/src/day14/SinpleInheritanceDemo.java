0d6e1800314625181f9c94619eb5e4882b09d4e0
ub-victor <victoireushindi371@gmail.com>
2025-11-12 17:07:49 +0200

    inheritace

--- MyJavaProject/src/day14/SinpleInheritanceDemo.java
+++ MyJavaProject/src/day14/SinpleInheritanceDemo.java
@@ -1,6 +1,17 @@
 package day14;
 
+class A{
+	int a;
+	
+	void display() {
+		System.out.println("In class A: "+ a);
+	}
+}
 
+class B extends A{
+	int b;
+	
+}
 
 
 public class SinpleInheritanceDemo {
