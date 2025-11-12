e06045a74843a78f388f0a1e3660c8c0b992d4f4
ub-victor <victoireushindi371@gmail.com>
2025-11-13 00:29:40 +0200

    inheritace

--- MyJavaProject/src/day14/HierarchicalInheritanceDemo.java
+++ MyJavaProject/src/day14/HierarchicalInheritanceDemo.java
@@ -30,7 +30,15 @@ public class HierarchicalInheritanceDemo {
 		
 		Child1 c1 = new Child1();
 		c1.display(10);
-		c1.show()
+		c1.show(20);
+		
+		Child2 c2 = new Child2();
+		c2.display(15);
+		c2.show(25);
+		c2.print(20);
+		
+		
+		
 		
 
 	}
