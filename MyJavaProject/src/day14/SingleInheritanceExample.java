7dacc5f4b23ba1cf694fd9f26600999af3df8f4c
ub-victor <victoireushindi371@gmail.com>
2025-11-12 17:19:50 +0200

    inheritace

--- MyJavaProject/src/day14/SingleInheritanceExample.java
+++ MyJavaProject/src/day14/SingleInheritanceExample.java
@@ -3,6 +3,10 @@ package day14;
 class Employee{
 	String name = "Victoire Ushindi";
 	int empID = 27269;
+	
+	void word() {
+		System.out.println(name + " Is working");
+	}
 }
 
 public class SingleInheritanceExample {
