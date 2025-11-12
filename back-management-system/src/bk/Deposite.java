829d8755a37cdb2908370194e75d4250d2d42970
ub-victor <victoireushindi371@gmail.com>
2025-11-12 19:47:25 +0200

    inheritace

--- back-management-system/src/bk/Deposite.java
+++ back-management-system/src/bk/Deposite.java
@@ -1,10 +1,50 @@
 package bk;
 
-public class Deposite {
+public class Deposite extends OpenAccount {
+		private int transactionId;
+		private double deposited;
+		private double finalBalance;
 
-	public static void main(String[] args) {
+		public Deposite(int bankId, String bankName, String bprAddress, int nationalId, String firstName,
+				String lastName, int age, String address, String phoneNumber, double initialBalance, int transactionId,
+				double deposited, double finalBalance) {
+			super(bankId, bankName, bprAddress, nationalId, firstName, lastName, age, address, phoneNumber, initialBalance);
+			this.transactionId = transactionId;
+			this.deposited = deposited;
+			this.finalBalance = finalBalance;
+			
+
 		
+		}
 
-	}
+		public int getTransactionId() {
+			return transactionId;
+		}
+
+		public void setTransactionId(int transactionId) {
+			this.transactionId = transactionId;
+		}
+
+		public double getDeposited() {
+			return deposited;
+		}
+
+		public void setDeposited(double deposited) {
+			this.deposited = deposited;
+		}
+
+		public double getFinalBalance() {
+			return finalBalance;
+		}
+
+		public void setFinalBalance(double finalBalance) {
+			this.finalBalance = finalBalance;
+		}
 
 }
+
+	
+		
+
+
+
