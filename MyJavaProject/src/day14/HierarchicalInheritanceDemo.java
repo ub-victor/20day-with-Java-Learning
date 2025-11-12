cdc7d1f974dcfc2c51025d8d3786533dfafe9369
ub-victor <victoireushindi371@gmail.com>
2025-11-12 20:02:04 +0200

    inheritace

--- MyJavaProject/src/day14/HierarchicalInheritanceDemo.java
+++ MyJavaProject/src/day14/HierarchicalInheritanceDemo.java
@@ -2,6 +2,13 @@ package day14;
 
 class Parent{
 	
+	void display(int a) {
+		System.out.println(a);
+	}
+	
+}
+
+class Child1 extends Parent{
 	
 }
 
--- back-management-system/src/bk/Main.java
+++ back-management-system/src/bk/Main.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Deposite deposite = new Deposite(101, "Global Bank", "123 Main St", 987654321, "John", "Doe", 30, "456 Elm St", "555-1234", 1000.0, 1, 500.0, 1500.0);
+		// Default values of integer type is 0 and for String type is null
+		Deposite deposite = new Deposite();
+		Scanner scanner = new Scanner(System.in);
+		deposite.setBankId(101);
 
 	}
 
