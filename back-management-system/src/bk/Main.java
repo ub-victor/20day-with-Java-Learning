2bdea9bad5444deaad3ecde64b0e63ee255c2659
ub-victor <victoireushindi371@gmail.com>
2025-11-12 20:12:36 +0200

    inheritace

--- back-management-system/src/bk/Main.java
+++ back-management-system/src/bk/Main.java
@@ -19,6 +19,8 @@ public class Main {
 		// Default values of integer type is 0 and for String type is null
 		Deposite deposite = new Deposite(idBack, bankName, null, 0, null, null, 0, null, null, 0.0, 0, 0.0, 0.0);
 		Scanner scanner = new Scanner(System.in);
+
+		
 		System.out.println("Enter Bank ID: ");
 		idBack = scanner.nextInt();
 
