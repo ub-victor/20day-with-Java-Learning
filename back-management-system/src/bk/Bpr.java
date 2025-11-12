57c468daac86cbaac04c5c24ed195c0398598a6a
ub-victor <victoireushindi371@gmail.com>
2025-11-12 19:29:08 +0200

    inheritace

---
+++ back-management-system/src/bk/Bpr.java
@@ -0,0 +1,18 @@
+package bk;
+
+public class Bpr {
+    private String bprAddress;
+
+
+    public Bpr(String bprAddress) {
+        this.bprAddress = bprAddress;
+    }
+
+    public String getBprAddress() {
+        return bprAddress;
+    }
+    public void setBprAddress(String bprAddress) {
+        this.bprAddress = bprAddress;
+    }
+
+}
