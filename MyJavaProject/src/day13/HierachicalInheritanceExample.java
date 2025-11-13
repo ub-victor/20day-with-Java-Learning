e4e383ca6739ff5b7714ce8ad23c1639b139072c
ub-victor <victoireushindi371@gmail.com>
2025-11-13 11:39:07 +0200

    inheritace

---
+++ MyJavaProject/src/day13/HierachicalInheritanceExample.java
@@ -0,0 +1,25 @@
+package day13;
+
+class Animal1{
+	String type = "General Animal";
+	
+	void eat() {
+		System.out.println("This animal eats food");
+	}
+}
+
+class Dog1 extends Animal1{
+	String breed = "Labrador";
+	
+	void bark() {
+		System.out.println("The dog barks");
+	}
+}
+
+public class HierachicalInheritanceExample {
+
+	public static void main(String[] args) {
+		 
+	}
+
+}
