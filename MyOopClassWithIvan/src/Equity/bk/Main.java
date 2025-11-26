0dacb6353b74e781c17617cad19d12ca08364abe
ub-victor <victoireushindi371@gmail.com>
2025-11-26 19:58:24 +0200

    Overriding

---
+++ MyOopClassWithIvan/src/Equity/bk/Main.java
@@ -0,0 +1,14 @@
+package Equity.bk;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter id");
+        AccountInfo accountInfo = new AccountInfo();
+        int in = sc.nextInt();
+    }
+    
+}
