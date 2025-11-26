aae81d0a6c796b79beae562416176b5cd1ef9d59
ub-victor <victoireushindi371@gmail.com>
2025-11-26 20:53:07 +0200

    Overriding

--- MyOopClassWithIvan/src/equity/bk/OpenAccountImp.java
+++ MyOopClassWithIvan/src/equity/bk/OpenAccountImp.java
@@ -4,7 +4,12 @@ public class OpenAccountImp implements OpenAccount  {
 
     @Override
     public void addAccount() {
-        
+        AccountInfo acc = null;
+        System.out.println("the national id is " + acc.getNationalId()
+        + " the first name is " + acc.getfName()
+        + " the last name is " + acc.getlName()
+        + " the age is " + acc.getAge()
+        + " the address is " + acc.getAddress());
     }
 
     @Override
