11191394b4b734b5fe680f6cb0db43daeaffa36d
ub-victor <victoireushindi371@gmail.com>
2025-11-26 13:58:05 +0200

    Overriding

--- MyJavaProject/src/day15/OverloadingVsOverriding.java
+++ MyJavaProject/src/day15/OverloadingVsOverriding.java
@@ -12,17 +12,17 @@ class ABC {
 
 class XYZ extends ABC{
 	void m1(int a ) { // Overriding
-		System.out.println(a);
+		System.out.println(a*a);
 		
 	}
 	
 	void m2(int b ) { // Overriding
-		System.out.println(b);
+		System.out.println(b*b);
 		
 	}
 	
-	void m2(int b ) { // Overriding
-		System.out.println(b);
+	void m2(int a, int b ) { // Overloading
+		System.out.println(a+b);
 		
 	}
 	
