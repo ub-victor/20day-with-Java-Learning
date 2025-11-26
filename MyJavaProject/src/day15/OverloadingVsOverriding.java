79d46053df61254aeaea8fe7d23c185d2dcb3b53
ub-victor <victoireushindi371@gmail.com>
2025-11-26 13:48:58 +0200

    Overriding

--- MyJavaProject/src/day15/OverloadingVsOverriding.java
+++ MyJavaProject/src/day15/OverloadingVsOverriding.java
@@ -1,5 +1,15 @@
 package day15;
 
+class ABC {
+	void m1(int a) {
+		System.out.println(a);
+	}
+	
+	void m2(int b) {
+		System.out.println(b);
+	}
+}
+
 public class OverloadingVsOverriding {
 
 	public static void main(String[] args) {
