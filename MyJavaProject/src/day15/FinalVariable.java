c2b3af9229fc5baf9b4c59cb319985816368f8e7
ub-victor <victoireushindi371@gmail.com>
2025-12-03 19:31:22 +0200

    Super in Java

--- MyJavaProject/src/day15/FinalVariable.java
+++ MyJavaProject/src/day15/FinalVariable.java
@@ -1,5 +1,12 @@
 package day15;
 
+class Parent{
+	final double PI = 3.14159;
+}
+class Child extends Parent{
+	double PI = 3.45443;
+}
+
 public class FinalVariable {
 	
 	public static void main(String[] args) {
