9df5c8ace012dae88171fb92c4cdcdeafceda1c2
ub-victor <victoireushindi371@gmail.com>
2025-12-11 15:19:38 +0200

    Interface

---
+++ MyJavaProject/src/day15/FinalMethod.java
@@ -0,0 +1,18 @@
+package day15;
+
+class BackAccount{
+	double balance;
+	
+	void BankAccount(double balance){
+		this.balance = balance;
+	}
+}
+
+public class FinalMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
