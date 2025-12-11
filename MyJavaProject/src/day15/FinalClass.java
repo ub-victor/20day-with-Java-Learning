fbdb32f10082e7a02d96be58807c796f269dfef0
ub-victor <victoireushindi371@gmail.com>
2025-12-11 16:21:42 +0200

    Interface

---
+++ MyJavaProject/src/day15/FinalClass.java
@@ -0,0 +1,16 @@
+package day15;
+
+class Vehical () {
+	void display() {
+		System.out.println("This is a vehicle");
+	}
+}
+
+public class FinalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
