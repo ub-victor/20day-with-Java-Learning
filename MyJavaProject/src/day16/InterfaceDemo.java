78f560c9b39d7c2693e36998c993faf55cb863b1
ub-victor <victoireushindi371@gmail.com>
2025-12-13 00:59:08 +0200

    Interface

---
+++ MyJavaProject/src/day16/InterfaceDemo.java
@@ -0,0 +1,26 @@
+package day16;
+
+interface Shape{
+	int length = 10;
+	int width = 20;
+	double PI = 3.14159;
+	
+	void circle();
+	
+	default void square() {
+		System.out.println("This is a Square -- default method");
+	}
+	
+	default void rectangle() {
+		System.out.println("This is a Rectangle -- default method");
+	}
+}
+
+public class InterfaceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
