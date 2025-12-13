47bf531f336a0e45fbc2b2ac5a01e11e887fbcef
ub-victor <victoireushindi371@gmail.com>
2025-12-13 02:27:32 +0200

    Interface

--- MyJavaProject/src/day16/Animal.java
+++ MyJavaProject/src/day16/Animal.java
@@ -2,6 +2,10 @@ package day16;
 
 abstract class Animal {
 	abstract void makeSound(); // abstract method
+	
+	void sleep() {
+		System.out.println("Animal is sleeping");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
