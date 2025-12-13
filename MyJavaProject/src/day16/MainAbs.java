a80c41f7f9b80904dd8334f9628ae9be438bc2bd
ub-victor <victoireushindi371@gmail.com>
2025-12-13 03:11:54 +0200

    Interface

--- MyJavaProject/src/day16/MainAbs.java
+++ MyJavaProject/src/day16/MainAbs.java
@@ -1,5 +1,39 @@
 package day16;
 
+//Abstract class
+abstract class Shapes {
+ abstract double area();  // abstract method
+}
+
+//Subclass 1: Circle
+class Circle extends Shapes {
+ double radius;
+
+ Circle(double radius) {
+     this.radius = radius;
+ }
+
+ @Override
+ double area() {
+     return 3.14 * radius * radius;
+ }
+}
+
+//Subclass 2: Rectangle
+class Rectangle extends Shapes {
+ double width;
+ double height;
+
+ Rectangle(double width, double height) {
+     this.width = width;
+     this.height = height;
+ }
+
+ @Override
+ double area() {
+     return width * height;
+ }
+}
 public class MainAbs {
 
 	public static void main(String[] args) {
