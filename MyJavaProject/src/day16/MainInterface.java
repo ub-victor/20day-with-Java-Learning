70ca52986085414a9354d0d1f7068bac1aece251
ub-victor <victoireushindi371@gmail.com>
2025-12-13 02:53:50 +0200

    Interface

--- MyJavaProject/src/day16/MainInterface.java
+++ MyJavaProject/src/day16/MainInterface.java
@@ -5,6 +5,38 @@ interface Vehicle {
 	void stop();
 }
 
+class Car implements Vehicle{
+
+	@Override
+	public void start() {
+		 System.out.println("Car is starting...");
+		
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("Car has stopped.");
+		
+	}
+	
+}
+
+class Bike implements Vehicle {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
 public class MainInterface {
 
 	public static void main(String[] args) {
