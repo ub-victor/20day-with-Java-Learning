4c8860fe7454a010a8dac6aacfbef58b4347720d
ub-victor <victoireushindi371@gmail.com>
2026-02-10 18:26:47 +0200

    Wrapper classes

--- MyJavaProject/src/day17/AutoBoxingExample.java
+++ MyJavaProject/src/day17/AutoBoxingExample.java
@@ -6,7 +6,7 @@ public class AutoBoxingExample {
 		
 		int marks = 90;
 		Integer score = marks; // autoboxing
-		
+		                                                                                                                                                                                                                                                                                          
 		System.out.println(score);
 
 	}
