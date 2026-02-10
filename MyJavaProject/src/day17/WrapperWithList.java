88f612c8e51af05beda93a2db7cd134f809ba61d
ub-victor <victoireushindi371@gmail.com>
2026-02-10 19:50:15 +0200

    Wrapper classes

--- MyJavaProject/src/day17/WrapperWithList.java
+++ MyJavaProject/src/day17/WrapperWithList.java
@@ -1,9 +1,13 @@
 package day17;
 
+import java.util.ArrayList;
+
 public class WrapperWithList {
 
 	public static void main(String[] args) {
 		
+		
+		ArrayList<Integer> numbers = new ArrayList<>();
 
 	}
 
