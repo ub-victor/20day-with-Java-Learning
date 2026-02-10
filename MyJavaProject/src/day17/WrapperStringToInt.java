05f2e7bbb022ab0edd0edfe4def1c8258a7469d8
ub-victor <victoireushindi371@gmail.com>
2026-02-10 18:41:40 +0200

    Wrapper classes

--- MyJavaProject/src/day17/WrapperStringToInt.java
+++ MyJavaProject/src/day17/WrapperStringToInt.java
@@ -3,8 +3,8 @@ package day17;
 public class WrapperStringToInt {
 
 	public static void main(String[] args) {
-		String age = '25';
-
+		String age = "25";
+		int num = Integer.parseInt(age);
 	}
 
 }
