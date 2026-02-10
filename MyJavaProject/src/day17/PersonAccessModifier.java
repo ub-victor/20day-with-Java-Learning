1d112e72ab3dbc8743c43cbd1494ce0d672ff7e7
ub-victor <victoireushindi371@gmail.com>
2026-02-10 20:05:56 +0200

    Wrapper classes

--- MyJavaProject/src/day17/PersonAccessModifier.java
+++ MyJavaProject/src/day17/PersonAccessModifier.java
@@ -4,11 +4,13 @@ public class PersonAccessModifier {
 	
 	public String name = "Ushindi Bihame Victoire";
 	private int age = 21;
+	
+	public void showAge(){
+		System.out.println(age);
+	}
 
 	public static void main(String[] args) {
-		public void showAge(){
-			
-		}
+		
 
 	}
 
