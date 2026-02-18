9a32c059444da40f0aa7124b1d0bdb14fc794a32
ub-victor <victoireushindi371@gmail.com>
2026-02-18 15:21:04 +0200

    StudentDemo

--- MyJavaProject/src/day17/Student.java
+++ MyJavaProject/src/day17/Student.java
@@ -4,12 +4,8 @@ public class Student {
 	
 	private int marks;
 	
-	public void setMarks(int m) {
-		marks = m;		
-	}
-	
-	public int getMarks() {
-		return marks;
+	public void setMarks() {
+		
 	}
 
 }
