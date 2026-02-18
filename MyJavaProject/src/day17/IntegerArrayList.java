6681ceeaebab9aa48b296ed2e17853eee2b12685
ub-victor <victoireushindi371@gmail.com>
2026-02-18 17:59:56 +0200

    Wrapper class

--- MyJavaProject/src/day17/IntegerArrayList.java
+++ MyJavaProject/src/day17/IntegerArrayList.java
@@ -5,7 +5,9 @@ public class IntegerArrayList {
 	public  static void main(String[] args) {
 		ArrayList<Integer> nums = new ArrayList<Integer>();
 		
-		num.add(10);
+		nums.add(10);
+		nums.add(10);
+		nums.add(10);
 	}
 
 	
