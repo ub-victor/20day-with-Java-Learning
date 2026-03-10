aa2e4059ed29b8788d2c1b0a9839db3d8ac6438e
ub-victor <victoireushindi371@gmail.com>
2026-03-10 11:10:41 +0200

    Java logging

--- MyJavaProject/src/logging/SimpleLogging.java
+++ MyJavaProject/src/logging/SimpleLogging.java
@@ -4,7 +4,12 @@ import java.util.logging.*;
 public class SimpleLogging {
 	
 	public static void main(String[] args) {
-		
+//		Create a Logger with a class name SimpleLogging
+		Logger logger = Logger.getLogger(SimpleLogging.class.getName());
+//		Call info method
+		logger.info("Message 1");
+		logger.info("Message 2");
+	
 	}
 	
 }
