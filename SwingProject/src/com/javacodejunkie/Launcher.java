86302e198a7d889737a98463d09a0acbdde93a90
ub-victor <victoireushindi371@gmail.com>
2026-03-18 11:39:21 +0200

    Swing

---
+++ SwingProject/src/com/javacodejunkie/Launcher.java
@@ -0,0 +1,18 @@
+package com.javacodejunkie;
+
+import javax.swing.SwingUtilities;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable(){
+			
+			
+			
+			
+		} );
+		
+
+	}
+
+}
---
+++ SwingProject/src/com/javacodejunkie/MainWindow.java
@@ -0,0 +1,5 @@
+package com.javacodejunkie;
+
+public class MainWindow {
+
+}
