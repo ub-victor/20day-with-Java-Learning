ece9cdc794a73349b9865a92d9a10196bf123838
ub-victor <victoireushindi371@gmail.com>
2026-03-25 01:42:36 +0200

    SchoolManagementSystem

--- School-Management-System/src/school/Person.java
+++ School-Management-System/src/school/Person.java
@@ -18,4 +18,32 @@ public class Person {
         this.age = age;
         this.address = address;
     }
+
+    // Getters and Setters (encapsulation)
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    
 }
