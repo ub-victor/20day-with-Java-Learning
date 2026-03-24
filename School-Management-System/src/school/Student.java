cff74ffad22d5754c4c984545efa4df82073d896
ub-victor <victoireushindi371@gmail.com>
2026-03-25 01:48:23 +0200

    SchoolManagementSystem

---
+++ School-Management-System/src/school/Student.java
@@ -0,0 +1,11 @@
+package school;
+
+/**
+ * Represents a student, extending Person.
+ * Adds student ID, grade level, and a list of courses the student is enrolled in
+ */
+
+public class Student {
+    
+    
+}
