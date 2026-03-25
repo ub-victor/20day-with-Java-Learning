d74a9316d666b6920013d27a960894bb19919295
ub-victor <victoireushindi371@gmail.com>
2026-03-25 02:01:45 +0200

    SchoolManagementSystem

--- School-Management-System/src/school/Student.java
+++ School-Management-System/src/school/Student.java
@@ -10,7 +10,8 @@ import java.util.List;
 public class Student extends Person {
     private String studentId;
     private String gradeLevel;
-    private List<Course> enrolledCourses;
+    // private List<Course> enrolledCourses;
+    
 
     public Student(String name, int age, String address, String studentId, String gradeLevel, String ){
         super(name, age, address);
