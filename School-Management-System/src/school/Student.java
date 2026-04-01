6bfbf730df549df182ba909bc9ca8b50d3386d53
ub-victor <victoireushindi371@gmail.com>
2026-04-01 19:31:41 +0200

    Student

--- School-Management-System/src/school/Student.java
+++ School-Management-System/src/school/Student.java
@@ -3,5 +3,8 @@ package school;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Represents a student, extending Person.
+ * Adds student ID, grade level, and a list of courses the student is enrolled in
+ */
 
